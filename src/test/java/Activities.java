75f25a0b96f5d51e4fb2b1960386c054326ae2b4
ADRIANO FANTINELLI <adriano.fantinelli-ext@viavarejo.com.br>
2020-10-19 09:56:37 -0300

    Initial commit

---
+++ src/test/java/Activities.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class Activities {
+
+    @Test
+    public void searchAllActivities(){
+        given().when().post("url").then();
+    }
+
+    @Test
+    public void createActivity(){
+        given().when().post("url").then();
+    }
+
+    @Test
+    public void deleteActivity(){
+        given().when().post("url").then();
+    }
+
+    @Test
+    public void searchActivity(){
+        given().when().post("url").then();
+    }
+
+    @Test
+    public void editActivity(){
+        given().when().post("url").then();
+    }
+
+}
