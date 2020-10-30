e2cfcb3f5b2fd9bbd17f8e05d470c6589b7b42d5
ADRIANO FANTINELLI <adriano.fantinelli-ext@viavarejo.com.br>
2020-10-30 17:32:05 -0300

    Continuando os testes

--- src/test/java/Activities.java
+++ src/test/java/Activities.java
@@ -1,17 +1,47 @@
 import org.junit.Test;
-
+import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 
 public class Activities {
 
     @Test
+    public void permissionApi(){
+        given()
+            .contentType("application/json")
+            .body("{\"email\": \"adrianofa2013@gmail.com\", \"appDescription\": \"Adriano Test\"}")
+        .when()
+            .post("https://api.thecatapi.com/v1/user/passwordlesssignup")
+        .then()
+            .statusCode(200)
+            .body("message", containsString("SUCCESS"))
+            .log().all();
+    }
+
+
+    @Test
     public void searchAllActivities(){
-        given().when().post("url").then();
+        given()
+                .when().get("http://fakerestapi.azurewebsites.net/api/Activities")
+                .then()
+                    .log().all();
     }
 
     @Test
     public void createActivity(){
-        given().when().post("url").then();
+        given()
+                .contentType("application/json")
+                .body("{" +
+                        "  \"ID\": 11,\n" +
+                        "  \"Title\": \"Play soccer\",\n" +
+                        "  \"DueDate\": \"2020-10-30T10:26:17.362Z\",\n" +
+                        "  \"Completed\": false\n" +
+                        "}")
+        .when()
+                .post("http://fakerestapi.azurewebsites.net/api/Activities")
+        .then()
+                .statusCode(200)
+//                .body("", containsString(""))
+                .log().all();
     }
 
     @Test
