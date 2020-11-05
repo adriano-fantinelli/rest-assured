4d9db51c8eaf40e9a3cb408aa0ff8c4fac7541dc
ADRIANO FANTINELLI <adriano.fantinelli-ext@viavarejo.com.br>
2020-11-05 16:34:58 -0300

    Creating schemas validations.

--- src/test/java/Activities.java
+++ src/test/java/Activities.java
@@ -1,29 +1,24 @@
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class Activities {
-
-    @Test
-    public void permissionApi(){
-        given()
-            .contentType("application/json")
-            .body("{\"email\": \"adrianofa2013@gmail.com\", \"appDescription\": \"Adriano Test\"}")
-        .when()
-            .post("https://api.thecatapi.com/v1/user/passwordlesssignup")
-        .then()
-            .statusCode(200)
-            .body("message", containsString("SUCCESS"))
-            .log().all();
+    @BeforeClass
+    public static void base(){
+        baseURI = "http://fakerestapi.azurewebsites.net/api/Activities/";
     }
-
 
     @Test
     public void searchAllActivities(){
         given()
-                .when().get("http://fakerestapi.azurewebsites.net/api/Activities")
-                .then()
-                    .log().all();
+        .when()
+                .get("")
+        .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas/activities/activities-schema.json"))
+                .log().all();
     }
 
     @Test
@@ -31,32 +26,55 @@ public class Activities {
         given()
                 .contentType("application/json")
                 .body("{" +
-                        "  \"ID\": 11,\n" +
+                        "  \"ID\": 31,\n" +
                         "  \"Title\": \"Play soccer\",\n" +
                         "  \"DueDate\": \"2020-10-30T10:26:17.362Z\",\n" +
                         "  \"Completed\": false\n" +
                         "}")
         .when()
-                .post("http://fakerestapi.azurewebsites.net/api/Activities")
+                .post("")
         .then()
                 .statusCode(200)
-//                .body("", containsString(""))
+                .body(matchesJsonSchemaInClasspath("schemas/activities/activity-schema.json"))
                 .log().all();
     }
 
     @Test
     public void deleteActivity(){
-        given().when().post("url").then();
+        given()
+        .when()
+                .delete("31")
+        .then()
+                .statusCode(200)
+                .log().all();
     }
 
     @Test
     public void searchActivity(){
-        given().when().post("url").then();
+        given()
+        .when()
+                .get("10")
+        .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas/activities/activity-schema.json"))
+                .log().all();
     }
 
     @Test
     public void editActivity(){
-        given().when().post("url").then();
+        given()
+                .contentType("application/json")
+                .body("{" +
+                        "  \"ID\": 10,\n" +
+                        "  \"Title\": \"Edited Activity\",\n" +
+                        "  \"DueDate\": \"2020-11-05T10:26:17.362Z\",\n" +
+                        "  \"Completed\": true\n" +
+                        "}")
+        .when()
+                .put("10")
+        .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas/activities/activity-schema.json"))
+                .log().all();
     }
-
 }
